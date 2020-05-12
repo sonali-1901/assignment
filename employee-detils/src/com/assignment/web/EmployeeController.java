ae7c08b7e0e21aa645118a6688356d5cf0980c8b
Ganesh Gaitonde <gaitonde@gmail.com>
2020-05-12 20:57:54 +0530

    [12-May-2020] commit for build using jenkins

--- employee-detils/src/com/assignment/web/EmployeeController.java
+++ employee-detils/src/com/assignment/web/EmployeeController.java
@@ -5,13 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.assignment.modals.Employee;
 import com.assignment.service.EmployeeService;
